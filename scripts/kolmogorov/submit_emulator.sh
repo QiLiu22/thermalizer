326743b362e240880c6cabe70d44db8dd848e291
CheeseCode <150875374+QiLiu22@users.noreply.github.com>
2025-03-01 16:36:16 -0500

    Update submit_emulator.sh

--- scripts/kolmogorov/submit_emulator.sh
+++ scripts/kolmogorov/submit_emulator.sh
@@ -12,7 +12,7 @@
 module purge
 
 singularity exec --nv \
-	    --overlay /scratch/cp3759/sing/overlay-50G-10M.ext3:ro \
+	    --overlay /scratch/ql2221/Qi_singularity/overlay-50G-10M.ext3:ro \
 	    /scratch/work/public/singularity/cuda11.1-cudnn8-devel-ubuntu18.04.sif \
-	    /bin/bash -c "source /ext3/env.sh; python3 /home/cp3759/Projects/thermalizer/scripts/kolmogorov/train_drnM.py"
+	    /bin/bash -c "source /ext3/env.sh; python3 /home/ql2221/Projects/Thermalizer/thermalizer/scripts/kolmogorov/train_drnM.py"
 
