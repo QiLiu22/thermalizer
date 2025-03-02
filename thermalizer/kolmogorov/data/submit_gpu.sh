95de4830461b4c74cb17bd0ab961d742796ce282
CheeseCode <150875374+QiLiu22@users.noreply.github.com>
2025-03-02 15:19:16 -0500

    Update submit_gpu.sh

--- thermalizer/kolmogorov/data/submit_gpu.sh
+++ thermalizer/kolmogorov/data/submit_gpu.sh
@@ -14,5 +14,6 @@ module purge
 singularity exec --nv \
 	    --overlay /scratch/ql2221/Qi_singularity/overlay-50G-10M.ext3:ro \
 	    /scratch/work/public/singularity/cuda12.1.1-cudnn8.9.0-devel-ubuntu22.04.2.sif \
-	    /bin/bash -c "source /ext3/env.sh; python3 /home/ql2221/Projects/Thermalizer/thermalizer/thermalizer/kolmogorov/data/run_kolmogorov.py --config data_config.yml --save_path /scratch/ql2221/thermalizer_data/kolmogorov/reynolds5k/emu1.p"
+	    /bin/bash -c "source /ext3/env.sh; python3 /home/ql2221/Projects/Thermalizer/thermalizer/thermalizer/kolmogorov/data/run_kolmogorov.py --config /home/ql2221/Projects/Thermalizer/thermalizer/thermalizer/kolmogorov/data/data_config.yml --save_path /scratch/ql2221/thermalizer_data/kolmogorov/reynolds5k/emu1.p
+"
 
